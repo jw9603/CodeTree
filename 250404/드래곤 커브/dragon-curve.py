17276400b0edf91cd8fbb5fa874aa616b61bf9fd
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-04-04 15:39:48 +0900

    Title: 드래곤 커브, Time: 72ms, Memory: 18MB, Status: Passed - Codetree

--- 250404/드래곤 커브/dragon-curve.py
+++ 250404/드래곤 커브/dragon-curve.py
@@ -1,26 +1,33 @@
-
 dir = [(0,1),(-1,0),(0,-1),(1,0)]  # 각각 동북서남
 
 N = int(input())
 graph = [[0]*101 for _ in range(101)]
-for _ in range(N):
-    x, y, d, g = map(int, input().split())  # 각각 좌표, 시작방향, 세대 수
-    graph[x][y] += 1
-    todo = [d]
-    x = x+dir[d][0]
-    y = y+dir[d][1]
-    graph[x][y] += 1
-    todo = [(d+1)%4]  # 이쪽의 turn까지 0세대 처리
 
-    for _ in range(g):  # 여기는 1세대부터 처리
-        for i in range(len(todo)):
-            x = x+dir[todo[i]][0]
-            y = y+dir[todo[i]][1]
-            graph[x][y] += 1
-        todo = todo[::-1]+todo
-        for i in range(len(todo)//2):
-            todo[i] += 1
-            todo[i] %= 4
+for _ in range(N):
+    x, y, d, g = map(int, input().split())
+    todo = [d]
+    
+    if 0 <= x < 101 and 0 <= y < 101:
+        graph[x][y] = 1
+
+    x += dir[d][0]
+    y += dir[d][1]
+
+    if 0 <= x < 101 and 0 <= y < 101:
+        graph[x][y] = 1
+
+    for _ in range(g):
+        temp = []
+        for i in reversed(todo):
+            temp.append((i + 1) % 4)
+        todo += temp
+
+        for i in temp:
+            x += dir[i][0]
+            y += dir[i][1]
+            if 0 <= x < 101 and 0 <= y < 101:
+                graph[x][y] = 1
+
 cnt = 0
 for i in range(100):
     for j in range(100):
