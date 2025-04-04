a560ea58a778240c95ed6b68a9d602a3f66d5058
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-04-04 15:30:58 +0900

    Title: 드래곤 커브, Time: 49ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250404/드래곤 커브/dragon-curve.py
+++ 250404/드래곤 커브/dragon-curve.py
@@ -1,6 +1,5 @@
 def main():
-    N = int(input().strip())
-
+    N = int(input())
     visited = [[False] * 101 for _ in range(101)]
 
     dx = [1, 0, -1, 0]
@@ -13,25 +12,22 @@ def main():
         for _ in range(g):
             for i in reversed(range(len(directions))):
                 directions.append((directions[i] + 1) % 4)
-        
-        points = [(x, y)]
+
+        visited[y][x] = True  # 시작점 마킹
+
         for direction in directions:
-            last_x, last_y = points[-1]
-            nx = last_x + dx[direction]
-            ny = last_y + dy[direction]
-            points.append((nx, ny))
-        
-        for px, py in points:
-            if 0 <= px <= 100 and 0 <= py <= 100:
-                visited[py][px] = True
-    
+            x += dx[direction]
+            y += dy[direction]
+            if 0 <= x <= 100 and 0 <= y <= 100:
+                visited[y][x] = True
+
     cnt = 0
     for i in range(100):
         for j in range(100):
             if visited[i][j] and visited[i + 1][j] and visited[i][j + 1] and visited[i + 1][j + 1]:
                 cnt += 1
-    
+
     print(cnt)
 
-if __name__ == '__main__':
-    main()     
+if __name__ == "__main__":
+    main()
