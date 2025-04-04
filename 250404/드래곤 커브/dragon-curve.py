262c642e5ba82e6779e3b1989b8b344b19abc24e
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2025-04-04 15:52:13 +0900

    Title: 드래곤 커브, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250404/드래곤 커브/dragon-curve.py
+++ 250404/드래곤 커브/dragon-curve.py
@@ -1,36 +1,34 @@
-dir = [(0,1),(-1,0),(0,-1),(1,0)]  # 각각 동북서남
+def main():
+    N = int(input().strip())
 
-N = int(input())
-graph = [[0]*101 for _ in range(101)]
+    visited = [[False] * 101 for _ in range(101)]
 
-for _ in range(N):
-    x, y, d, g = map(int, input().split())
-    todo = [d]
+    dx = [0, -1, 0, 1]
+    dy = [1, 0, -1, 0]
+
+    for _ in range(N):
+        x, y, d, g = map(int, input().split())
+
+        directions = [d]
+        for _ in range(g):
+            for i in reversed(range(len(directions))):
+                directions.append((directions[i] + 1) % 4)
+        
+        visited[x][y] = True
+        for direction in directions:
+            x += dx[direction]
+            y += dy[direction]
+
+            if 0 <= x <= 100 and 0 <= y <= 100:
+                visited[x][y] = True
     
-    if 0 <= x < 101 and 0 <= y < 101:
-        graph[x][y] = 1
+    cnt = 0
+    for i in range(100):
+        for j in range(100):
+            if visited[i][j] and visited[i + 1][j] and visited[i][j + 1] and visited[i + 1][j + 1]:
+                cnt += 1
+    
+    print(cnt)
 
-    x += dir[d][0]
-    y += dir[d][1]
-
-    if 0 <= x < 101 and 0 <= y < 101:
-        graph[x][y] = 1
-
-    for _ in range(g):
-        temp = []
-        for i in reversed(todo):
-            temp.append((i + 1) % 4)
-        todo += temp
-
-        for i in temp:
-            x += dir[i][0]
-            y += dir[i][1]
-            if 0 <= x < 101 and 0 <= y < 101:
-                graph[x][y] = 1
-
-cnt = 0
-for i in range(100):
-    for j in range(100):
-        if graph[i][j] and graph[i+1][j] and graph[i][j+1] and graph[i+1][j+1]:
-            cnt += 1
-print(cnt)
+if __name__ == '__main__':
+    main()  
